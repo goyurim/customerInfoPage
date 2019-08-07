a558078f87c892180db5cda540b80598c2606855
goyurim <qpeijty1@gmail.com>
2019-08-08 00:59:24 +0900

    add to test file

---
+++ app/src/test/java/com/example/qpeij/qrcameratest/initionalActivity.java
@@ -0,0 +1,12 @@
+package com.example.qpeij.qrcameratest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class initionalActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initional);
+    }
+}
